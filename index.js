056fb2e9cc41ac17b0d48d53e5fc80ae6a043851
kelly r maass <kmaass77627@gmail.com>
2020-09-12 05:23:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let main = document.body.querySelector('main#main');
+main.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.className = "victory";
+newHeader.id = "victory"
+document.body.appendChild(newHeader);
+newHeader.innerHTML = "is the champion";
