a2d19746ba9ada20edb2a64b686f5c2ebff63d37
kelly r maass <kmaass77627@gmail.com>
2020-09-12 05:16:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 let main = document.body.querySelector('main#main');
 main.remove();
 
-let newHeader = document.createElement('h1#victory');
+let newHeader = document.createElement('h1');
+newHeader.className = "victory";
 document.body.appendChild(newHeader);
