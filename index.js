7af24b1ca093efc4c00a958035974a2874af8430
kelly r maass <kmaass77627@gmail.com>
2020-09-12 05:23:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ main.remove();
 
 let newHeader = document.createElement('h1');
 newHeader.className = "victory";
+newHeader.id = "victory"
 document.body.appendChild(newHeader);
 newHeader.innerHTML = "is the champion";
